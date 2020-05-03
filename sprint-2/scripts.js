ab31f463623173f20b7e02a40a7c426cb6d60341
Nikki <aletheanicole.n@gmail.com>
2020-05-03 13:31:47 -0400

    Add default comments

--- sprint-2/scripts.js
+++ sprint-2/scripts.js
@@ -1,4 +1,73 @@
 
+// Comments
+let comments = [
+    {
+        pic: '../assets/image/asia.jpg',
+        name: 'Theodore Duncan',
+        date: '11/15/2018',
+        comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
+    },
+    {
+        pic: '../assets/image/chico.jpg',
+        name: 'Gary Wong',
+        date: '12/12/2018',
+        comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
+    },
+    {
+        pic: '../assets/image/cat.jpg',
+        name: 'Michael Lyons',
+        date: '12/18/2018',
+        comment: 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
+    }
+];
+
+let commentsSection = document.querySelector('.comments__container');
+
+const displayComments = (c) => {
+    let commentCard = document.createElement('div');
+    commentCard.classList.add('comments__card');
+
+    let commentContainer = document.createElement('div');
+    commentContainer.classList.add('comments__item');
+
+    let pic = document.createElement('img');
+    pic.classList.add('comments__pic');
+    pic.src = c.pic;
+
+    let commentList = document.createElement('ul');
+    commentList.classList.add('comments__list');
+
+    let name = document.createElement('li');
+    name.classList.add('comments__name');
+    name.innerText = c.name;
+
+    let date = document.createElement('li');
+    date.classList.add('comments__date');
+    date.innerText = c.date;
+
+    let comment = document.createElement('p');
+    comment.classList.add('comments__text');
+    comment.innerText = c.comment;
+
+    commentList.appendChild(name);
+    commentList.appendChild(date);
+    commentContainer.appendChild(commentList);
+    commentContainer.appendChild(comment);
+    commentCard.appendChild(pic);
+    commentCard.appendChild(commentContainer);
+    commentsSection.appendChild(commentCard);
+}
+
+comments.forEach(c => {
+    displayComments(c);
+});
+
+
+
+
+
+
+
 
 // Shows
 const shows = [
@@ -42,8 +111,6 @@ const shows = [
 
 let container = document.querySelector('.shows__table');
 
-// let headerContainer
-
 let showsHeader = ['DATES', 'VENUE', 'LOCATION'];
 
 let head = document.createElement('div');
@@ -57,8 +124,8 @@ const displayHead = (h) => {
     showHeader.innerText = h;
 
     head.appendChild(showHeader);
-    container.appendChild(head)
-}
+    container.appendChild(head);
+};
 
 showsHeader.forEach(h => {
     displayHead(h);
