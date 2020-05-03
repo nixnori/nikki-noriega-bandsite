8dc8df5554e037d859264e9b3caa33750d83a21d
Nikki <aletheanicole.n@gmail.com>
2020-05-03 00:46:24 -0400

    Style mobile and tablet view for Shows page

--- sprint-2/scripts.js
+++ sprint-2/scripts.js
@@ -40,34 +40,72 @@ const shows = [
     }
 ];
 
-const showHeaders = ['DATE','VENUE', 'LOCATION'];
-
-console.log(shows[1]);
-
 let container = document.querySelector('.shows__table');
 
-function displayTable(show) {
+// let headerContainer
+
+let showsHeader = ['DATES', 'VENUE', 'LOCATION'];
+
+let head = document.createElement('div');
+head.classList.add('shows__card');
+head.classList.add('header__container');
+
+const displayHead = (h) => {
+
+    let showHeader = document.createElement('h4');
+    showHeader.classList.add('table__header--1')
+    showHeader.innerText = h;
+
+    head.appendChild(showHeader);
+    container.appendChild(head)
+}
+
+showsHeader.forEach(h => {
+    displayHead(h);
+});
+
+
+const displayTable = show => {
     let card = document.createElement('div');
     card.classList.add('shows__card');
 
+    let showDateHeader = document.createElement('h4');
+    showDateHeader.classList.add('table__header')
+    showDateHeader.innerText = 'DATE';
+
     let showDate = document.createElement('h4');
+    showDate.classList.add('table__item')
     showDate.innerText = show.date;
 
+    let showVenueHeader = document.createElement('h4');
+    showVenueHeader.classList.add('table__header')
+    showVenueHeader.innerText = 'VENUE';
+
     let showVenue = document.createElement('p');
+    showVenue.classList.add('table__item')
     showVenue.innerText = show.venue;
 
+    let showLocationHeader = document.createElement('h4');
+    showLocationHeader.classList.add('table__header')
+    showLocationHeader.innerText = 'LOCATION';
+
     let showLocation = document.createElement('p');
+    showLocation.classList.add('table__item')
     showLocation.innerText = show.location;
 
     let showButton = document.createElement('button');
+    showButton.classList.add('shows__button');
     showButton.innerText = show.button;
 
+    card.appendChild(showDateHeader);
     card.appendChild(showDate);
+    card.appendChild(showVenueHeader);
     card.appendChild(showVenue);
+    card.appendChild(showLocationHeader);
     card.appendChild(showLocation);
     card.appendChild(showButton);
     container.appendChild(card);
-}
+};
 
 shows.forEach(show => {
     displayTable(show);
